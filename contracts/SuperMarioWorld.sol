22e73c74338ac227dbd53229fe587cee894ac7f3
IMPWNG <matisse.ach+github@gmail.com>
2022-02-02 17:57:45 -0500

    Create SuperMarion collection contract +  mint function
    Collection (name, symbol, tokenCount, tokenURI)
    Mint function

---
+++ contracts/SuperMarioWorld.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.2;
+
+import "./ERC721.sol";
+
+contract SuperMarioWorld is ERC721 {
+
+    string public name;
+
+    string public symbol;
+
+    uint256 public tokenCount;
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(
+        string memory _name, 
+        string memory _symbol
+    ) { 
+        name = _name;
+        symbol = _symbol;
+    }
+
+    // Returns an URL that points to the metadata
+    function tokenURI(uint256 tokenId) public returns (string memory) {
+        require(_owner[tokenId] =! address(0), "TokenId does not exist");
+        return _tokenURIs[tokenId];
+
+    }
+
+    // This create a new NFT inside our collection
+    function mint(string memory _tokenURI) public {
+        
+        tokenCount += 1; // tokenId
+        _balances[msg.sender] += 1;
+        _owners[tokenCount] = msg.sender;
+        _tokenURIs[tokenCount] = _tokenURI;
+
+        emit Transfer(address(0),  msg.sender, tokenCount);
+    }
+
+    // Mint function
+    // support interface function
+}
